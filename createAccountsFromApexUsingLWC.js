a85ead10e0922b5a7de95e59a27923eeefa22cb6
tejasri15 <51012931+tejasri15@users.noreply.github.com>
2023-11-04 15:41:54 -0500

    Creation & page navigation for accounts using apex

--- createAccountsFromApexUsingLWC.js
+++ createAccountsFromApexUsingLWC.js
@@ -1,11 +1,13 @@
 import { LightningElement, api, track, wire } from 'lwc';
-import addAccounts from '@salesforce/apex/crudSampleExamples.addAccounts'
+import addAccounts from '@salesforce/apex/crudSampleExamples.addAccounts';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class CreateAccountsFromApexUsingLWC extends LightningElement {
+export default class CreateAccountsFromApexUsingLWC extends NavigationMixin(LightningElement) {
 
     @track accountNameValue='';
     @track descripitionValue='';
-    @track accountId;
+     myVariable;
+    variable1;
 
     nameChangedValue(event){
         this.name= event.target.value;
@@ -17,27 +19,49 @@ export default class CreateAccountsFromApexUsingLWC extends LightningElement {
 
     onSaveButton(){
         console.log('name value getting for save button' + this.name);
-        addAccounts({accountName:this.name})
+        this.gettingNewlyCreatedId(this.name);
+        if(this.myVariable!=null || this.myVariable != undefined){
+            console.log("Entered into if condition");
+            this.navigateToRecordPage(this.myVariable);
+            console.log("if condition closing here");
+        }
+    }
+
+    gettingNewlyCreatedId(name){
+        console.log(name);
+        variable1 = addAccounts({accountName:name})
         .then(result=>{
-            this.accountId= result;
-            console.log("data from success stage" + this.accountId + "    " + result);
-            resetFunctionality();
+            this.myVariable= result;
+            console.log("data from success stage" + this.myVariable + "    " + result);
+            this.navigateToRecordPage(this.myVariable);
         })
         .catch(error=>{
             this.erorr = error;
         });
-        handleSomeEvent();
+       
     }
-
     handleSomeEvent() {
         console.log("entered here");
-        console.log('Value of accountId: ' + this.accountId);
     }
 
     resetFunctionality(){
+        console.log('Value of accountId: ' + this.myVariable);
         this.accountNameValue=" ";
         this.descripitionValue=" ";
     }
 
+
+    navigateToRecordPage(myVariable) {
+        console.log("Entered into method");
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: myVariable,
+                objectApiName: 'Account', 
+                actionName: 'view',
+            },
+        });
+        console.log("Method closing here");
+    }
 
 }
